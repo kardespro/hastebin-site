731c2d0405e489263278c76841fea8d0dfb688cb
Glitch (hello-express) <none>
2021-08-15 18:38:03 +0000

    🌟🚐 Checkpoint ./README.md:31988211/1192 ./server.js:31988211/1584 ./package.json:31988211/2605

--- README.md
+++ README.md
@@ -1,30 +1,14 @@
-# hello-express
-
-A server that serves a webpage, its resources, and some data
+# Hastebin Site
 
 
-## Your Project
-
-On the front-end,
-
-- Edit `views/index.html` to change the content of the webpage
-- `public/client.js` is the javacript that runs when you load the webpage
-- `public/style.css` is the styles for `views/index.html`
-- Drag in `assets`, like images or music, to add them to your project
-
-On the back-end,
-
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env` (nobody can see this but you and people you invite)
-
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy.
+Bu Altyapı Nego#0001 Tarafından Yazıldı
 
 
-## Made by [Glitch](https://glitch.com/)
 
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
+Altyapida Bulunanlar
 
-Find out more [about Glitch](https://glitch.com/about).
-
-( ᵔ ᴥ ᵔ )
++ Yazi Ekle
++ Yazi Dili
++ Yazi Goruntule
++ Raw
++ JSON API
--- server.js
+++ server.js
@@ -1,34 +1,50 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+const {
+    JsonDatabase,
+    YamlDatabase
+} = require("wio.db");
 
-// our default array of dreams
+const db = new JsonDatabase({
+  databasePath:"./Database/paste.json"
+});
 const dreams = [
   "Find and count some sheep",
   "Climb a really tall mountain",
   "Wash the dishes"
 ];
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
+app.set('view engine', 'ejs');
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.render("home");
+  
+});
+app.post("/", (req, res) => {
+//Paste ID
+    function make_paste_id(length) {
+    var result = "";
+    var characters =
+      "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  var negoB = req.body;
+  var Data = {
+    author: negoB["author"],
+    paste: negoB["paste"],
+    paste_language: negoB["pastelang"]
+  };
+  db.push(`paste`);
+  
+
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
